9bc615e02d64033bbe153d72fa4afef3b9ffd370
ecker91 <ecker91@gmail.com>
2025-06-23 20:14:25 -0300

    correcao backend

--- my-office-api/database/ConexaoMySql.js
+++ my-office-api/database/ConexaoMySql.js
@@ -1,11 +1,12 @@
 import mysql from 'mysql2';
 
 const db = mysql.createConnection({
-    host: process.env.MYSQL_HOST || "localhost",
-    port: process.env.MYSQL_PORT || "3307",
-    user: process.env.MYSQL_USER || "root",
-    password: process.env.MYSQL_PWD || "17527530",
-    database: process.env.MYSQL_DB || "salasdb",
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER, 
+    password: process.env.MYSQL_PWD,
+    database: process.env.MYSQL_DB,
+    dateStrings: true,
   });
   
   db.connect((err) => {
